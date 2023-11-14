88043716608d07129324d7cf8c0e6d48568d1aa5
Patrik Pukan <pukp00@vse.cz>
2023-11-14 15:22:42 +0100

    feat: edit POM for version 1.0.0, gitlab CI config

--- src/main/java/cz/vse/pukp00/jedifx/HomeController.java
+++ src/main/java/cz/vse/pukp00/jedifx/HomeController.java
@@ -1,11 +1,17 @@
 package cz.vse.pukp00.jedifx;
 
+import cz.vse.pukp00.jedifx.game.Game;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.Optional;
 
 public class HomeController {
+
+    @FXML
+    private Button tlacitkoOdesli;
 
     @FXML
     private TextArea vystup;
@@ -13,9 +19,40 @@ public class HomeController {
     @FXML
     private TextField vstup;
 
+    private Game game = new Game();
+
+    @FXML
+    private void initialize() {
+        vystup.appendText(game.getPrologue()+"\n\n");
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vstup.requestFocus();
+            }
+        });
+    }
     @FXML
     private void odesliVstup(ActionEvent actionEvent) {
-        vystup.appendText(vstup.getText()+"\n");
+        String prikaz = vstup.getText();
+        vystup.appendText("> " +prikaz+"\n");
+        String vysledek = game.process(prikaz);
+        vystup.appendText(vysledek+"\n\n");
         vstup.clear();
+
+        if (game.isGameOver()) {
+            vystup.appendText(game.getEpilogue());
+            vstup.setDisable(true);
+            tlacitkoOdesli.setDisable(true);
+
+        }
+    }
+
+    public void ukoncitHru(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Chces naozaj ukoncit hru?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            Platform.exit();
+        }
+        //Platform.exit();
     }
 }
