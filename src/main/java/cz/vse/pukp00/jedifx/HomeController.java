60f83a90cc5fb9ac038abf80c229de4e168a57d8
Patrik Pukan <pukp00@vse.cz>
2023-11-15 02:36:14 +0100

    feat: implement visual moving

--- src/main/java/cz/vse/pukp00/jedifx/HomeController.java
+++ src/main/java/cz/vse/pukp00/jedifx/HomeController.java
@@ -7,13 +7,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.*;
 
 public class HomeController {
 
+    @FXML
+    private ImageView hrac;
     @FXML
     private ListView<Area> panelVychodu;
     @FXML
@@ -29,6 +36,8 @@ public class HomeController {
 
     private ObservableList<Area> seznamVychodu = FXCollections.observableArrayList();
 
+    private Map<String, Point2D> souradniceProstoru = new HashMap<>();
+
     @FXML
     private void initialize() {
         vystup.appendText(game.getPrologue()+"\n\n");
@@ -39,15 +48,39 @@ public class HomeController {
             }
         });
         panelVychodu.setItems(seznamVychodu);
-        game.getWorld().registruj(ZmenaHry.ZMENA_MISTNOSTI, () -> aktualizujSeznamVychodu());
+        game.getWorld().registruj(ZmenaHry.ZMENA_MISTNOSTI, () -> {
+            aktualizujSeznamVychodu();
+            aktualizujPolohuHrace();
+        });
         game.registruj(ZmenaHry.KONEC_HRY, () -> aktualizujKonecHry());
         aktualizujSeznamVychodu();
+        vlozSouradnice();
+        panelVychodu.setCellFactory(param -> new ListCellArea());
+    }
+
+    private void vlozSouradnice() {
+        souradniceProstoru.put("lod", new Point2D(6,105));
+        souradniceProstoru.put("strom", new Point2D(88,105));
+        souradniceProstoru.put("domcek", new Point2D(160,105));
+        souradniceProstoru.put("dzungla", new Point2D(92,195));
+        souradniceProstoru.put("cistinka", new Point2D(190,200));
+        souradniceProstoru.put("brloh", new Point2D(310,200));
+        souradniceProstoru.put("jazero", new Point2D(90,25));
+        souradniceProstoru.put("jaskyna", new Point2D(180,25));
+        souradniceProstoru.put("chodba", new Point2D(280,25));
+        souradniceProstoru.put("radioveza", new Point2D(380,30));
     }
 
     @FXML
     private void aktualizujSeznamVychodu() {
         seznamVychodu.clear();
         seznamVychodu.addAll(game.getWorld().getCurrentArea().getExits());
+    }
+
+    private void aktualizujPolohuHrace(){
+        String menoProstoru = game.getWorld().getCurrentArea().getName();
+        hrac.setLayoutX(souradniceProstoru.get(menoProstoru).getX());
+        hrac.setLayoutY(souradniceProstoru.get(menoProstoru).getY());
     }
 
     private void aktualizujKonecHry() {
@@ -89,7 +122,7 @@ public class HomeController {
     private void klikPanelVychodu(MouseEvent mouseEvent) {
         Area cil =  panelVychodu.getSelectionModel().getSelectedItem();
         if (cil==null) return;
-        String prikaz = "jdi " +cil;
+        String prikaz = "jdi " +cil.getName();
        // game.process(prikaz);
         zpracujPrikaz(prikaz);
     }
---
+++ src/main/java/cz/vse/pukp00/jedifx/ListCellArea.java
@@ -0,0 +1,23 @@
+package cz.vse.pukp00.jedifx;
+
+import cz.vse.pukp00.jedifx.game.Area;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
+
+public class ListCellArea extends ListCell<Area> {
+    @Override
+    protected void updateItem(Area area, boolean empty) {
+        super.updateItem(area, empty);
+
+        if (empty) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            setText(area.getName());
+            ImageView iw = new ImageView(getClass().getResource("prostory/"+area.getName()+".jpg").toExternalForm());
+            iw.setFitWidth(150);
+            iw.setPreserveRatio(true);
+            setGraphic(iw);
+        }
+    }
+}
